3a67248458eb21e4fb9ca1cf2cf515f4c7daa8d5
geekyzk <geekyzk@163.com>
2017-11-29 14:00:03 +0800

    1

--- src/main/java/com/em248/novel/spider/AbstractBookSpider.java
+++ src/main/java/com/em248/novel/spider/AbstractBookSpider.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,19 +23,21 @@ abstract class AbstractBookSpider {
 
     protected Document pageDocument;
 
+    protected String novelPageUrl;
+
     public List<String> searchTemplateOne(String s, String bookName) {
         try {
-            Console.log("Request search url: {}",String.format(searchTemplateUrlOne, s, bookName));
+            Console.log("Request search url: {}", String.format(searchTemplateUrlOne, s, bookName));
             Document document = Jsoup.connect(String.format(searchTemplateUrlOne, s, bookName)).get();
             Elements bookList = document.getElementsByClass("result-item-title result-game-item-title");
-            List<String> result =  bookList.stream()
+            List<String> result = bookList.stream()
                     .filter(element -> {
                         Element a = element.getElementsByTag("a").first();
                         return bookName.equals(a.attr("title"));
                     })
                     .map(e -> e.getElementsByTag("a").first().attr("abs:href"))
                     .collect(Collectors.toList());
-            Console.log("search result : {}",result);
+            Console.log("search result : {}", result);
             return result;
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,6 +49,7 @@ abstract class AbstractBookSpider {
         Element info = document.getElementById("info");
         String name = info.getElementsByTag("h1").first().text();
         String author = info.getElementsByTag("p").first().text();
+
         return BookInfoDto.builder().author(author)
                 .createDate(new Date())
                 .sourceUrl(document.baseUri())
@@ -54,32 +58,60 @@ abstract class AbstractBookSpider {
     }
 
     public List<BookChapterDto> bookChapterTemplateOne(Document document) {
+        List<BookChapterDto> result = new ArrayList<>();
         Element list = document.getElementById("list");
         Elements chapterList = list.getElementsByTag("dd");
-        for(int i = 0 ; i < chapterList.size();i++) {
-            
+        for (int i = 0; i < chapterList.size(); i++) {
+            Element element = chapterList.get(i);
+            String name = element.child(0).text();
+            String sourceUrl = element.child(0).absUrl("href");
+            BookChapterDto bookChapter = BookChapterDto.builder()
+                    .creaetTime(new Date())
+                    .index(i + 1)
+                    .name(name)
+                    .sourceUrl(sourceUrl)
+                    .build();
+            result.add(bookChapter);
         }
+        return result;
     }
 
-
+    public List<BookChapterDto> bookChapterContentTemplateOne(List<BookChapterDto> bookChapterDtos) {
+        List<BookChapterDto> bookChapterList = new ArrayList<>();
+        for (BookChapterDto bookChapterDto : bookChapterDtos) {
+            if (bookChapterDto.getSourceUrl() == null) {
+                Console.log("Chapter [{}] the sourceUrl is null", bookChapterDto.getName());
+            }
+            try {
+                Document document = Jsoup.connect(bookChapterDto.getSourceUrl()).get();
+                Element content = document.getElementById("content");
+                bookChapterDto.setContent(content.text());
+                bookChapterList.add(bookChapterDto);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return bookChapterList;
+    }
 
 
     abstract List<String> searchBook(String bookName);
 
 
     public void novelPage(String bookUrl) {
-         novelPage(bookUrl, 3);
+        novelPage(bookUrl, 3);
     }
 
-    public void novelPage(String bookUrl,Integer reConnect){
+    public void novelPage(String bookUrl, Integer reConnect) {
         try {
-          this.pageDocument =  Jsoup.connect(bookUrl).get();
+            this.pageDocument = Jsoup.connect(bookUrl).get();
+            this.novelPageUrl = bookUrl;
         } catch (IOException e) {
             if (reConnect != null && reConnect >= 1) {
-                Console.log(e,"Get [{}] page error,reconnect {}",bookUrl,reConnect);
+                Console.log(e, "Get [{}] page error,reconnect {}", bookUrl, reConnect);
                 novelPage(bookUrl, --reConnect);
-            }else{
-                Console.log(e,"Get [{}] error",bookUrl);
+            } else {
+                Console.log(e, "Get [{}] error", bookUrl);
                 this.pageDocument = null;
             }
         }
